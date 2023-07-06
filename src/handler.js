9455b9cda619733e4e876dc14649db248d2dfaf8
kelvin hernandez cabrera <kelhzkb@hotmail.com>
2023-07-06 09:12:00 +0200

    Lab

--- src/handler.js
+++ src/handler.js
@@ -1,8 +1,12 @@
 'use strict';
 
-module.exports.handler = async (event) => {
-  console.log('Hello World: ', event);
+module.exports.handler = async (event, context) => {
+
+  const message  = `Hello ${event.firstName} ${event.lastName}! Keep being awesome!`;
+  console.log(message);
+
   return {
     statusCode: 200,
+    message,
   };
 };
