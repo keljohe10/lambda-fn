9f0d6596a35cee252d36fc52b0104a993b423542
kelvin hernandez cabrera <kelhzkb@hotmail.com>
2023-08-15 08:02:12 +0200

    Deploy lambda-sqs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-module.exports.handler = async (event, context) => {
+module.exports.handler = async (event) => {
 
-  const message  = `Hello ${event.firstName} ${event.lastName}! Keep being awesome!`;
+  const message  = `Hello ${event.id} ${event.name}! Keep being awesome!`;
   console.log(message);
 
   return {
